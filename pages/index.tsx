e2354c16f09b9646385e0f78703120204af43142
alyanna <mcgratha@oxy.edu>
2023-06-15 20:43:15 -0400

    custom color to 'applied' rows

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import Cell from '@/components/Cell';
-import styles from '@/styles/pages/Index.module.scss';
-import { useEffect, useMemo, useState } from 'react';
-import Select from 'react-select';
+import Cell from "@/components/Cell";
+import styles from "@/styles/pages/Index.module.scss";
+import { useEffect, useMemo, useState } from "react";
+import Select from "react-select";
 
-const url = 'https://raw.githubusercontent.com/pittcsc/Summer2024-Internships/dev/README.md';
-const separator = '<!-- Please leave a one line gap between this and the table -->\n';
+const url =
+  "https://raw.githubusercontent.com/pittcsc/Summer2024-Internships/dev/README.md";
+const separator =
+  "<!-- Please leave a one line gap between this and the table -->\n";
 
 type Internship = {
   name: string;
@@ -14,48 +16,52 @@ type Internship = {
 };
 
 function parseName(text: string) {
-  if (text.includes('](')) {
-    return text.split('](')[0].slice(1);
+  if (text.includes("](")) {
+    return text.split("](")[0].slice(1);
   }
 
   return text;
 }
 
 const appliedTypeOptions = [
-  { value: 'all', label: 'All' },
-  { value: 'yes', label: 'Applied' },
-  { value: 'no', label: 'Not Applied' },
+  { value: "all", label: "All" },
+  { value: "yes", label: "Applied" },
+  { value: "no", label: "Not Applied" },
 ];
 
 export default function Index() {
   const [internships, setInternships] = useState<Internship[] | null>(null);
   const [darkMode, setDarkMode] = useState(false);
   const [flipped, setFlipped] = useState(false);
-  const [filterText, setFilterText] = useState('');
-  const [appliedType, setAppliedType] = useState(appliedTypeOptions[0])
+  const [filterText, setFilterText] = useState("");
+  const [appliedType, setAppliedType] = useState(appliedTypeOptions[0]);
+  const [selectedColor, setSelectedColor] = useState("#999999"); // adding color
 
   // filter internships by filter text
   const filteredInternships = useMemo(() => {
-    if (!internships) return null
+    if (!internships) return null;
     const newInternships = internships.filter((internship) => {
       const { name, location, notes, applied } = internship;
-      const text = filterText.toLowerCase()
-      const textMatch = !filterText ||
-        (name.toLowerCase().includes(text) ||
-          location.toLowerCase().includes(text) ||
-          notes.toLowerCase().includes(text))
-      const appliedMatch = appliedType.value === 'all' ||
-        ((appliedType.value === 'yes') === applied)
-      return textMatch && appliedMatch
-    })
-    if (flipped) newInternships.reverse()
-    return newInternships
+      const text = filterText.toLowerCase();
+      const textMatch =
+        !filterText ||
+        name.toLowerCase().includes(text) ||
+        location.toLowerCase().includes(text) ||
+        notes.toLowerCase().includes(text);
+      const appliedMatch =
+        appliedType.value === "all" ||
+        (appliedType.value === "yes") === applied;
+      return textMatch && appliedMatch;
+    });
+    if (flipped) newInternships.reverse();
+    return newInternships;
   }, [internships, filterText, appliedType, flipped]);
 
   // initialize settings on start
   useEffect(() => {
-    setDarkMode(window.localStorage.getItem('dark-mode') === 'yes')
-    setFlipped(window.localStorage.getItem('flipped') === 'yes')
+    setDarkMode(window.localStorage.getItem("dark-mode") === "yes");
+    setFlipped(window.localStorage.getItem("flipped") === "yes");
+    setSelectedColor(window.localStorage.getItem("selectedColor") || "#999");
   }, []);
 
   // fetch data from github
@@ -63,22 +69,29 @@ export default function Index() {
     setInternships(null);
     const response = await fetch(url);
     if (!response.ok) {
-      throw new Error('An error occurred fetching data');
+      throw new Error("An error occurred fetching data");
     }
     const text = await response.text();
     const content = text.split(separator)[1].trim();
-    const lines = content.split('\n').slice(2);
-    const jobs = lines.map(line => line.split('|').map(text => text.trim()).filter(text => !!text));
-    setInternships(jobs.map((job) => ({
-      name: job[0],
-      location: job[1],
-      notes: job[2],
-      applied: getApplied(job[0])
-    })));
+    const lines = content.split("\n").slice(2);
+    const jobs = lines.map((line) =>
+      line
+        .split("|")
+        .map((text) => text.trim())
+        .filter((text) => !!text)
+    );
+    setInternships(
+      jobs.map((job) => ({
+        name: job[0],
+        location: job[1],
+        notes: job[2],
+        applied: getApplied(job[0]),
+      }))
+    );
 
     function getApplied(name: string) {
       const jobName = parseName(name);
-      return window.localStorage.getItem(`Applied: ${jobName}`) === 'yes';
+      return window.localStorage.getItem(`Applied: ${jobName}`) === "yes";
     }
   }
 
@@ -91,112 +104,157 @@ export default function Index() {
   function updateApplied(applied: boolean, internship: Internship) {
     if (!internships) return;
     const newInternships = internships.slice();
-    const index = newInternships.findIndex(i => i.name === internship.name);
+    const index = newInternships.findIndex((i) => i.name === internship.name);
     if (index === -1) return;
     newInternships[index].applied = applied;
     setInternships(newInternships);
 
     // update local storage
     const jobName = parseName(internship.name);
-    window.localStorage.setItem(`Applied: ${jobName}`, applied ? 'yes' : 'no');
+    window.localStorage.setItem(`Applied: ${jobName}`, applied ? "yes" : "no");
   }
 
   function toggleDarkMode() {
     const isDarkMode = !darkMode;
     setDarkMode(isDarkMode);
-    window.localStorage.setItem('dark-mode', isDarkMode ? 'yes' : 'no');
+    window.localStorage.setItem("dark-mode", isDarkMode ? "yes" : "no");
   }
 
   function toggleFlipped() {
     const isFlipped = !flipped;
     setFlipped(isFlipped);
-    window.localStorage.setItem('flipped', isFlipped ? 'yes' : 'no');
+    window.localStorage.setItem("flipped", isFlipped ? "yes" : "no");
   }
 
   return (
-    <div className={darkMode ? `${styles.container} ${styles.darkMode}` : styles.container}>
+    <div
+      className={
+        darkMode ? `${styles.container} ${styles.darkMode}` : styles.container
+      }
+    >
       <h1>interning.dev</h1>
       <p>
-        ⚠️ Not affiliated with{' '}
-        <a href="https://pittcsc.org/" target="_blank" rel="noopener noreferrer">PittCSC</a>
+        ⚠️ Not affiliated with{" "}
+        <a
+          href="https://pittcsc.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          PittCSC
+        </a>
       </p>
       <ul>
         <li>
-          Data from{' '}
-          <a href="https://github.com/pittcsc/Summer2024-Internships" target="_blank" rel="noopener noreferrer">
+          Data from{" "}
+          <a
+            href="https://github.com/pittcsc/Summer2024-Internships"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             PittCSC
           </a>
         </li>
         <li>
-          This dashboard is open source!{' '}
-          <a href="https://github.com/csaye/interning.dev" target="_blank" rel="noopener noreferrer">
+          This dashboard is open source!{" "}
+          <a
+            href="https://github.com/csaye/interning.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Star us on GitHub
           </a>
         </li>
         <li>
-          Made by{' '}
-          <a href="https://github.com/csaye" target="_blank" rel="noopener noreferrer">
+          Made by{" "}
+          <a
+            href="https://github.com/csaye"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Cooper Saye
           </a>
         </li>
       </ul>
       <div className={styles.buttons}>
-        <button onClick={() => toggleFlipped()}>
-          {flipped ? '⬆️' : '⬇️'}
-        </button>
+        <button onClick={() => toggleFlipped()}>{flipped ? "⬆️" : "⬇️"}</button>
         <button onClick={() => toggleDarkMode()}>
-          {darkMode ? '☀️' : '🌙'}
+          {darkMode ? "☀️" : "🌙"}
         </button>
-        <button
-          className={styles.refreshButton}
-          onClick={() => getData()}
-        >
+        <button className={styles.refreshButton} onClick={() => getData()}>
           🔄
         </button>
       </div>
-      {internships && <p>You have applied to <b>{internships.filter(i => i.applied).length}/{internships.length}</b> internships!</p>}
+      {internships && (
+        <p>
+          You have applied to{" "}
+          <b>
+            {internships.filter((i) => i.applied).length}/{internships.length}
+          </b>{" "}
+          internships!
+        </p>
+      )}
       <div className={styles.filters}>
         <input
           value={filterText}
-          onChange={e => setFilterText(e.target.value)}
+          onChange={(e) => setFilterText(e.target.value)}
           placeholder="Filter by text..."
         />
         <Select
           options={appliedTypeOptions}
           value={appliedType}
-          onChange={value => {
-            if (value) setAppliedType(value)
+          onChange={(value) => {
+            if (value) setAppliedType(value);
           }}
         />
+        <label>
+          Select a color for checked rows:{"  "}
+          <input
+            className={styles.colorPicker}
+            type="color"
+            value={selectedColor}
+            onChange={(event) => {
+              setSelectedColor(event.target.value);
+              window.localStorage.setItem("selectedColor", event.target.value);
+            }}
+          />
+        </label>
       </div>
-      {
-        !filteredInternships ? <p>Loading...</p> :
-          !filteredInternships.length ? <p>No internships found</p> :
-            <div className={styles.table}>
-              <div className={styles.row}>
-                <div>Name</div>
-                <div>Location</div>
-                <div>Notes</div>
-                <div>Applied</div>
-              </div>
-              {
-                filteredInternships.map((internship, i) =>
-                  <div className={styles.row} key={i}>
-                    <Cell text={internship.name} />
-                    <Cell text={internship.location} />
-                    <Cell text={internship.notes} />
-                    <div>
-                      <input
-                        checked={internship.applied}
-                        onChange={e => updateApplied(e.target.checked, internship)}
-                        type="checkbox"
-                      />
-                    </div>
-                  </div>
-                )
+      {!filteredInternships ? (
+        <p>Loading...</p>
+      ) : !filteredInternships.length ? (
+        <p>No internships found</p>
+      ) : (
+        <div className={styles.table}>
+          <div className={styles.row}>
+            <div>Name</div>
+            <div>Location</div>
+            <div>Notes</div>
+            <div>Applied</div>
+          </div>
+          {filteredInternships.map((internship, i) => (
+            <div
+              className={`${styles.row} ${
+                internship.applied ? styles.checkedRow : ""
+              }`}
+              key={i}
+              style={
+                internship.applied ? { backgroundColor: selectedColor } : {}
               }
+            >
+              <Cell text={internship.name} />
+              <Cell text={internship.location} />
+              <Cell text={internship.notes} />
+              <div>
+                <input
+                  checked={internship.applied}
+                  onChange={(e) => updateApplied(e.target.checked, internship)}
+                  type="checkbox"
+                />
+              </div>
             </div>
-      }
+          ))}
+        </div>
+      )}
     </div>
   );
 }
