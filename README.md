46fd179ac4147b65f05d54f44e143e8c671eefdc
Cooper Saye <coopersaye@gmail.com>
2023-06-16 23:40:35 -0400

    Style files with prettier

--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 
 Data source: [github.com/pittcsc/Summer2024-Internships](https://github.com/pittcsc/Summer2024-Internships)
 
-<sub>*⚠️ Not affiliated with [PittCSC](https://pittcsc.org/)*</sub>
+<sub>_⚠️ Not affiliated with [PittCSC](https://pittcsc.org/)_</sub>
--- components/Cell.tsx
+++ components/Cell.tsx
@@ -1,4 +1,4 @@
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from "react-markdown";
 
 type Props = {
   text: string;
@@ -7,17 +7,17 @@ type Props = {
 export default function Cell(props: Props) {
   const { text } = props;
 
-  const parsedText = text.replaceAll(/<br\s*\/>/g, '\n\n');
+  const parsedText = text.replaceAll(/<br\s*\/>/g, "\n\n");
 
   return (
     <div>
       <ReactMarkdown
         components={{
-          a: props => (
+          a: (props) => (
             <a href={props.href} target="_blank" rel="noopener noreferrer">
               {props.children}
             </a>
-          )
+          ),
         }}
         linkTarget="_blank"
       >
